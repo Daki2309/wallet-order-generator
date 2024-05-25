4a8a939c7c1223f76fc38eca3b5fdfcce68b1393
piksenia <x.billinger94@gmail.com>
2024-05-25 16:11:33 +0200

    read template to change given attributes dynamically or set default value, remove initialization via constructor

--- index.js
+++ index.js
@@ -3,92 +3,68 @@ let fs = require('file-system');
 const forge = require('node-forge');
 const AdmZip = require('adm-zip');
 
-async function generateOrder(createdAtDate, updatedAtDate, orderStatus, stationsAddressStreet, stationAddressCity,
-                             stationAddressPostalCode, paymentStatus, totalAmount, totalAmountCurrency, cardNumber,
-                             cardNumberMaskedSign, signerKeyPass) {
-    let order = await getOrderInstance(createdAtDate, updatedAtDate, orderStatus, stationsAddressStreet,
-        stationAddressCity, stationAddressPostalCode, paymentStatus, totalAmount, totalAmountCurrency, cardNumber,
-        cardNumberMaskedSign);
+const orderGenerator = {
+    async from() {
+        let orderUnconverted = await fs.fs.readFileSync('./model.order/order.json');
+        return JSON.parse(orderUnconverted, 'utf-8');
+    },
 
-    const zipFile = new AdmZip();
+    async generateOrder(updatedOrderJson, signerKeyPass) {
+        let order = updatedOrderJson;
 
-    zipFile.addFile("order.json", JSON.stringify(order), "", "");
+        const zipFile = new AdmZip();
 
-    const iconFile = await fs.fs.readFileSync('./model.order/icon.png');
+        zipFile.addFile("order.json", JSON.stringify(order), "", "");
 
-    let manifestJson = {
-        "icon.png" : createHash('sha256').update(iconFile).digest('hex'),
-        "order.json" : createHash('sha256').update(JSON.stringify(order)).digest('hex')
+        const iconFile = await fs.fs.readFileSync('./model.order/icon.png');
+
+        let manifestJson = {
+            "icon.png": createHash('sha256').update(iconFile).digest('hex'),
+            "order.json": createHash('sha256').update(JSON.stringify(order)).digest('hex')
+        }
+
+        zipFile.addFile("manifest.json", JSON.stringify(manifestJson), "", "");
+        zipFile.addLocalFile('./model.order/icon.png');
+
+        const signerCert = await fs.fs.readFileSync('./certs/signerCert.pem');
+        const signerCertCertificate = forge.pki.certificateFromPem(signerCert);
+        const signerKey = await fs.fs.readFileSync('./certs/signerKey.pem');
+        const signerKeyCertificate = forge.pki.decryptRsaPrivateKey(signerKey, signerKeyPass)
+        const wwdr = await fs.fs.readFileSync('./certs/wwdr.pem');
+        const wwdrCertificate = forge.pki.certificateFromPem(wwdr);
+
+        let p7 = forge.pkcs7.createSignedData();
+
+        p7.content = forge.util.createBuffer(
+            JSON.stringify(manifestJson),
+            "utf8",
+        );
+
+        p7.addCertificate(wwdrCertificate);
+        p7.addCertificate(signerCertCertificate);
+        p7.addSigner({
+            key: signerKeyCertificate,
+            certificate: signerCertCertificate,
+            digestAlgorithm: forge.pki.oids.sha256,
+            authenticatedAttributes: [
+                {
+                    type: forge.pki.oids.contentType,
+                    value: forge.pki.oids.data
+                },
+                {
+                    type: forge.pki.oids.messageDigest,
+                },
+                {
+                    type: forge.pki.oids.signingTime,
+                }
+            ]
+        });
+        await p7.sign({detached: true});
+        const signature = Buffer.from(forge.asn1.toDer(p7.toAsn1()).getBytes(), "binary");
+
+        zipFile.addFile("signature", signature, "", "");
+
+        return zipFile
     }
-
-    zipFile.addFile("manifest.json", JSON.stringify(manifestJson), "", "");
-    zipFile.addLocalFile('./model.order/icon.png');
-
-    const signature = await getSignature(signerKeyPass, manifestJson);
-    zipFile.addFile("signature", signature, "", "");
-
-    return zipFile
 }
-
-async function getOrderInstance(createdAtDate, updatedAtDate, orderStatus, stationsAddressStreet, stationAddressCity,
-                                stationAddressPostalCode, paymentStatus, totalAmount, totalAmountCurrency, cardNumber,
-                                cardNumberMaskedSign) {
-    let orderUnconverted = await fs.fs.readFileSync('./model.order/order.json');
-
-    let order = JSON.parse(orderUnconverted);
-
-    order.createdAt = createdAtDate;
-    order.updatedAt = updatedAtDate;
-    order.status = orderStatus;
-    //merchant information
-    order.merchant.address.addressLines = [stationsAddressStreet];
-    order.merchant.address.locality = stationAddressCity;
-    order.merchant.address.postalCode = stationAddressPostalCode;
-    //payment information
-    order.payment.status = paymentStatus;
-    order.payment.total.amount = totalAmount;
-    order.payment.total.currency = totalAmountCurrency;
-    order.payment.paymentMethods = [`${cardNumberMaskedSign} ${cardNumber}`]
-
-    return order;
-}
-
-async function getSignature(signerKeyPass, manifestJson) {
-    const signerCert = await fs.fs.readFileSync('./certs/signerCert.pem');
-    const signerCertCertificate = forge.pki.certificateFromPem(signerCert);
-    const signerKey = await fs.fs.readFileSync('./certs/signerKey.pem');
-    const signerKeyCertificate = forge.pki.decryptRsaPrivateKey(signerKey, signerKeyPass)
-    const wwdr = await fs.fs.readFileSync('./certs/wwdr.pem');
-    const wwdrCertificate = forge.pki.certificateFromPem(wwdr);
-
-    let p7 = forge.pkcs7.createSignedData();
-
-    p7.content = forge.util.createBuffer(
-        JSON.stringify(manifestJson),
-        "utf8",
-    );
-
-    p7.addCertificate(wwdrCertificate);
-    p7.addCertificate(signerCertCertificate);
-    p7.addSigner({
-        key: signerKeyCertificate,
-        certificate: signerCertCertificate,
-        digestAlgorithm: forge.pki.oids.sha256,
-        authenticatedAttributes: [
-            {
-                type: forge.pki.oids.contentType,
-                value: forge.pki.oids.data
-            },
-            {
-                type: forge.pki.oids.messageDigest,
-            },
-            {
-                type: forge.pki.oids.signingTime,
-            }
-        ]
-    });
-    await p7.sign({ detached: true });
-    return Buffer.from(forge.asn1.toDer(p7.toAsn1()).getBytes(), "binary");
-}
-
-module.exports = generateOrder
+module.exports = orderGenerator
