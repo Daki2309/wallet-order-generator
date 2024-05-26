6827e00ef05d003724d9ba3ff23cd91a004d57d4
piksenia <x.billinger94@gmail.com>
2024-05-26 18:01:16 +0200

    adjust test cases, outsource signature creation to seperate file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { createHash } = require('crypto');
 let fs = require('file-system');
-const forge = require('node-forge');
 const AdmZip = require('adm-zip');
+const signatureCreator = require("./signatureCreator");
 
 const orderGenerator = {
     /**
@@ -32,55 +32,6 @@ const orderGenerator = {
                 const array = itemName.split('.');
                 return array[array.length - 1] === 'png';
             });
-    },
-
-    /**
-     * generate signature
-     *
-     * @param {{}} manifestJson - contains a dictionary of the SHA-256 hashes for each of the source files for the order.
-     * The dictionary key is the pathname of the file relative to the top level of the order, and the value is the SHA-256 hash.
-     * @param {string} signerKeyPass - passphrase of signer key for decryption
-     * @param {string} [filePath = './certs'] - load certificate from given optional filepath from project root level,
-     * default from certs folder from project root level
-     *
-     * @returns {any} signature
-     */
-    async getSignature(manifestJson, signerKeyPass, filePath = './certs') {
-        const signerCert = await fs.fs.readFileSync(`${filePath}/signerCert.pem`);
-        const signerCertCertificate = forge.pki.certificateFromPem(signerCert);
-        const signerKey = await fs.fs.readFileSync(`${filePath}/signerKey.pem`);
-        const signerKeyCertificate = forge.pki.decryptRsaPrivateKey(signerKey, signerKeyPass)
-        const wwdr = await fs.fs.readFileSync(`${filePath}/wwdr.pem`);
-        const wwdrCertificate = forge.pki.certificateFromPem(wwdr);
-
-        let p7 = forge.pkcs7.createSignedData();
-
-        p7.content = forge.util.createBuffer(
-            JSON.stringify(manifestJson),
-            "utf8",
-        );
-
-        p7.addCertificate(wwdrCertificate);
-        p7.addCertificate(signerCertCertificate);
-        p7.addSigner({
-            key: signerKeyCertificate,
-            certificate: signerCertCertificate,
-            digestAlgorithm: forge.pki.oids.sha256,
-            authenticatedAttributes: [
-                {
-                    type: forge.pki.oids.contentType,
-                    value: forge.pki.oids.data
-                },
-                {
-                    type: forge.pki.oids.messageDigest,
-                },
-                {
-                    type: forge.pki.oids.signingTime,
-                }
-            ]
-        });
-        await p7.sign({detached: true});
-        return Buffer.from(forge.asn1.toDer(p7.toAsn1()).getBytes(), "binary");
     },
 
     /**
@@ -120,7 +71,7 @@ const orderGenerator = {
 
         zipFile.addFile("manifest.json", JSON.stringify(manifestJson), "", "");
 
-        const signature = await this.getSignature(manifestJson, signerKeyPass, certFolderPath);
+        const signature = await signatureCreator.getSignature(manifestJson, signerKeyPass, certFolderPath);
 
         zipFile.addFile("signature", signature, "", "");
 
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,15 @@
 const orderGenerator = require('../index.js');
+const signerCreator = require('../signatureCreator.js');
 
 test('from: get json', () => {
-    let result = orderGenerator.from('./tests/source/');
+    let result = orderGenerator.from('./tests/source');
 
     expect(result).not.toBeNull();
     expect(result).not.toBeUndefined();
 });
 
 test('getListOfPngFiles: filter out json file', () => {
-    let result = orderGenerator.getListOfPngFiles('./tests/source/');
+    let result = orderGenerator.getListOfPngFiles('./tests/source');
 
     expect(result.length).toEqual(4);
     expect(result.includes('bakery_logo.png')).toBeTruthy();
@@ -16,4 +17,114 @@ test('getListOfPngFiles: filter out json file', () => {
     expect(result.includes('pastry.png')).toBeTruthy();
     expect(result.includes('spongecake.png')).toBeTruthy();
     expect(result.includes('order.json')).toBeFalsy();
+});
+
+test('generateOrder fail: use default folder path ./model.order for template folder', async () => {
+    let orderInstance = orderGenerator.from('./tests/source');
+    try {
+        await orderGenerator.generateOrder(orderInstance, "123456789")
+    } catch (e) {
+        expect(e.code).toEqual('ENOENT');
+        expect(e.path).toEqual('./model.order/');
+    }
+});
+
+test('generateOrder: use specified folder path ./tests/source for template folder', async () => {
+    const mock = jest.spyOn(signerCreator, 'getSignature');
+    mock.mockImplementation(() => true);
+
+    let orderInstance = orderGenerator.from('./tests/source');
+
+    await orderGenerator.generateOrder(orderInstance, "123456789", undefined, './tests/source')
+
+    expect(mock).toHaveBeenCalledWith({
+            "bakery_logo.png": "80fac49e058e813ebfd974e5dd77002fb7ddcb1b931e64fc014c7b3f2227bb2e",
+            "cheesecake.png": "468339dbe7f0674416f90dd94f62d2015595d71ec95eff6232cb34d7d2f91d6b",
+            "order.json": "0dfaab96a1f1d1e75743e77f5f2e848942b0d97c562e46b55fc811b3c7f14743",
+            "pastry.png": "0865ac234f5c779e99e652b19a65b2b898b42475b91ffaf24c6a2767767c2fd1",
+            "spongecake.png": "3d4e67f90f7a669cfbc58ddf7dfcb13150ee85773901b1103a3896b2e5354fd5"
+        },
+        '123456789',
+        './certs')
+});
+
+test('generateOrder: use default includePngFileNames to import all available png files', async () => {
+    const mock = jest.spyOn(signerCreator, 'getSignature');
+    mock.mockImplementation(() => true);
+
+    const getListOfPngFilesMock = jest.spyOn(orderGenerator, 'getListOfPngFiles');
+
+    let orderInstance = orderGenerator.from('./tests/source');
+
+    await orderGenerator.generateOrder(orderInstance, "123456789", undefined, './tests/source')
+
+    expect(getListOfPngFilesMock).toHaveBeenCalled();
+});
+
+test('generateOrder: use given includePngFileNames to import only given png file name', async () => {
+    const mock = jest.spyOn(signerCreator, 'getSignature');
+    mock.mockImplementation(() => true);
+
+    const getListOfPngFilesMock = jest.spyOn(orderGenerator, 'getListOfPngFiles');
+
+    let orderInstance = orderGenerator.from('./tests/source');
+
+    await orderGenerator.generateOrder(orderInstance, "123456789", ['pastry.png'], './tests/source')
+
+    expect(getListOfPngFilesMock).not.toHaveBeenCalled();
+
+    expect(mock).toHaveBeenCalledWith({
+            "order.json": "0dfaab96a1f1d1e75743e77f5f2e848942b0d97c562e46b55fc811b3c7f14743",
+            "pastry.png": "0865ac234f5c779e99e652b19a65b2b898b42475b91ffaf24c6a2767767c2fd1"
+        },
+        '123456789',
+        './certs')
+});
+
+test('generateOrder fail: use default folder path ./certs for certs folder', async () => {
+    const mock = jest.spyOn(signerCreator, 'getSignature');
+    mock.mockImplementation(() => {
+        const error = new Error("no such file or directory");
+        error.path = './certs/signerCert.pem';
+        error.code = "ENOENT";
+        throw error;
+    });
+
+    let orderInstance = orderGenerator.from('./tests/source');
+
+    try {
+        await orderGenerator.generateOrder(orderInstance, "123456789", undefined, './tests/source')
+    } catch (e) {
+        expect(e.code).toEqual('ENOENT');
+        expect(e.path).toEqual('./certs/signerCert.pem');
+    }
+
+    expect(mock).toHaveBeenCalledWith({
+        "bakery_logo.png": "80fac49e058e813ebfd974e5dd77002fb7ddcb1b931e64fc014c7b3f2227bb2e",
+        "cheesecake.png": "468339dbe7f0674416f90dd94f62d2015595d71ec95eff6232cb34d7d2f91d6b",
+        "order.json": "0dfaab96a1f1d1e75743e77f5f2e848942b0d97c562e46b55fc811b3c7f14743",
+        "pastry.png": "0865ac234f5c779e99e652b19a65b2b898b42475b91ffaf24c6a2767767c2fd1",
+        "spongecake.png": "3d4e67f90f7a669cfbc58ddf7dfcb13150ee85773901b1103a3896b2e5354fd5"
+        },
+        '123456789',
+        './certs')
+});
+
+test('generateOrder fail: use specified folder path ./tests/source for certs folder', async () => {
+    const mock = jest.spyOn(signerCreator, 'getSignature');
+    mock.mockImplementation(() => true);
+
+    let orderInstance = orderGenerator.from('./tests/source');
+
+    await orderGenerator.generateOrder(orderInstance, "123456789", undefined, './tests/source', './tests/source')
+
+    expect(mock).toHaveBeenCalledWith({
+            "bakery_logo.png": "80fac49e058e813ebfd974e5dd77002fb7ddcb1b931e64fc014c7b3f2227bb2e",
+            "cheesecake.png": "468339dbe7f0674416f90dd94f62d2015595d71ec95eff6232cb34d7d2f91d6b",
+            "order.json": "0dfaab96a1f1d1e75743e77f5f2e848942b0d97c562e46b55fc811b3c7f14743",
+            "pastry.png": "0865ac234f5c779e99e652b19a65b2b898b42475b91ffaf24c6a2767767c2fd1",
+            "spongecake.png": "3d4e67f90f7a669cfbc58ddf7dfcb13150ee85773901b1103a3896b2e5354fd5"
+        },
+        '123456789',
+        './tests/source')
 });
